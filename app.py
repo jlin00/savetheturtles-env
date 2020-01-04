61fcd5f4be5796d734ab4a7f7233ca1ffc8647f6
Jackie Lin <jlin00@stuy.edu>
2020-01-04 09:58:45 -0500

    finished login

---
+++ app.py
@@ -0,0 +1,117 @@
+from flask import Flask , render_template,request, redirect, url_for, session, flash
+from functools import wraps
+import sqlite3, os
+from utl import db_builder, db_manager
+import random
+
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
+#====================================================
+def login_required(f):
+    """Decorator for making sure user is logged in"""
+    @wraps(f)
+    def dec(*args, **kwargs):
+        '''dec (*args, **kwargs): Decorator for checking login and if user in session'''
+        if 'username' in session:
+            for arg in args:
+                print(arg)
+            return f(*args, **kwargs)
+        flash('You must be logged in to view this page!', 'alert-danger')
+        return redirect('/')
+    return dec
+
+def no_login_required(f):
+    """Decorator for making sure user is not logged in"""
+    @wraps(f)
+    def dec(*args, **kwargs):
+        '''dec(*args, **kwargs): Decorator for checking no login'''
+        if 'username' not in session:
+            return f(*args, **kwargs)
+        flash('You cannot view this page while logged in!', 'alert-danger')
+        return redirect('/home')
+    return dec
+#====================================================
+
+@app.route("/")
+def root():
+    '''def root(): Redirects to login page if not in session, redirects to home if in session'''
+    if 'username' in session:
+        return redirect('/home')
+    return redirect('/login')
+
+@app.route("/login")
+@no_login_required
+def login():
+    '''def login(): login requirement'''
+    return render_template('login.html')
+
+@app.route("/auth", methods=["POST"])
+@no_login_required
+def auth():
+    '''auth(): authenticating login and flashing corresponding errors'''
+    enteredU = request.form['username']
+    enteredP = request.form['password']
+    if(enteredU == "" or enteredP == ""): #if fields empty
+        flash('Please fill out all fields!', 'alert-danger')
+        return redirect(url_for('login'))
+    if (db_manager.userValid(enteredU, enteredP)): #returns true if login successful
+        flash('You were successfully logged in!', 'alert-success')
+        session['username'] = enteredU
+        return redirect(url_for('home'))
+    else:
+        flash('Wrong Credentials!', 'alert-danger')
+        return redirect(url_for('login'))
+
+@app.route("/signup")
+@no_login_required
+def signup():
+    '''def signup(): sign up route, takes in a form for signing up'''
+    return render_template("signup.html")
+
+@app.route("/signupcheck", methods=["POST"])
+@no_login_required
+def signupcheck():
+    '''signupcheck(): Checking if sign up form is filled out correctly; i.e. username taken, passwords match, all fields filled out'''
+    username = request.form['username']
+    password = request.form['password']
+    confirm = request.form['confirmation']
+    #preliminary checks on entered fields
+    if(username == "" or password == "" or confirm == ""):
+        flash('Please fill out all fields!', 'alert-danger')
+        return render_template("signup.html", username=username, password=password, confirm=confirm)
+    if ("," in username):
+            flash('Commas are not allowed in username!', 'alert-danger')
+            return render_template("signup.html", username=username, password=password, confirm=confirm)
+    if (confirm!=password):
+        flash('Passwords do not match!', 'alert-danger')
+        return render_template("signup.html", username=username, password=password, confirm=confirm)
+    #form information delivered to backend
+    added = db_manager.addUser(username,password) #returns True if user was sucessfully added
+    if (not added):
+        flash('Username taken!', 'alert-danger')
+        return render_template("signup.html", username=username, password=password, confirm=confirm)
+    flash('You have successfully created an account! Please log in!', 'alert-success')
+    return redirect(url_for('login'))
+
+#====================================================
+#STARTING FROM HERE USER MUST BE LOGGED IN
+
+@app.route("/home")
+@login_required
+def home():
+    '''home(): homepage checks if user is in session and gets info on user'''
+    return "HOME"
+
+#====================================================
+@app.route("/logout")
+@login_required
+def logout():
+    '''def logout(): logging out of session, redirects to login page'''
+    session.clear()
+    flash('You were successfully logged out.', 'alert-success')
+    return redirect('/')
+
+if __name__ == "__main__":
+    db_builder.build_db()
+    app.debug = True
+    app.run()
---
+++ utl/db_manager.py
@@ -0,0 +1,33 @@
+import sqlite3
+from utl.db_builder import exec, execmany
+
+#====================================================
+#sign up and login functions
+
+#authenticates user
+def userValid(username,password):
+    '''def userValid(username,password): determines if username is in database and password corresponds to username'''
+    q = "SELECT username FROM user_tbl;"
+    data = exec(q)
+    for uName in data:
+        if uName[0] == username:
+            q = "SELECT password from user_tbl WHERE username=?"
+            inputs = (username,)
+            data = execmany(q, inputs)
+            for passW in data:
+                if (passW[0] == password):
+                    return True
+    return False
+
+#add user-provided credentials to database
+def addUser(username, password):
+    '''def addUser(username, password): adding user from sign up, taking in form inputs and writing to data table'''
+    q = "SELECT * FROM user_tbl WHERE username=?"
+    inputs = (username,)
+    data = execmany(q, inputs).fetchone()
+    if (data is None):
+        q = "INSERT INTO user_tbl VALUES(?, ?, '', 500, 0, '')"
+        inputs = (username, password)
+        execmany(q, inputs)
+        return True
+    return False #if username already exists
